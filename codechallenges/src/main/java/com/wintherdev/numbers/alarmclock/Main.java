2815669cd825f7f386332392f155ae787bacc6ac
Nicklas W. <Nicklas2002@hotmail.com>
2024-07-21 16:14:10 +0200

    Calculator WIP

---
+++ codechallenges/src/main/java/com/wintherdev/numbers/alarmclock/Main.java
@@ -0,0 +1,10 @@
+package com.wintherdev.numbers.alarmclock;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        AlarmClock alarmClock = new AlarmClock();
+
+    }
+}
